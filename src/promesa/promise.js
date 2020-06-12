51bbdcea8b7b4bcebfe4e1219e3e9ba2e5df1f9f
SchatzM <bri@npass.us>
2020-06-12 23:29:23 +0200

    Refactorizada parte del código

--- src/promesa/promise.js
+++ src/promesa/promise.js
@@ -9,13 +9,13 @@ let XMLHttpRequest = require ('xmlhttprequest').XMLHttpRequest;
 const API = 'https://rickandmortyapi.com/api/character/';
 
 let traerDatos = (url_api) => { // Función que incluirá la promesa
-		let xhttp = new XMLHttpRequest(); // Variable con la instancia que usaremos para las peticiones
+		let xhttp = new XMLHttpRequest (); // Variable con la instancia que usaremos para las peticiones
 		return new Promise ((resolve, reject) => { // Promesa
 			xhttp.open ('GET', url_api, true); // Hacemos la petición con el método GET a la url dada y de forma asíncrona
 			xhttp.onreadystatechange = () => { // Evento que se ejecuta cuando el estado de la petición cambia
 				if (xhttp.readyState === 4) { // Si el estado de dicha petición se ha completado
 					if (xhttp.status === 200) { // Y el código de estado de ésta es 200 (OK/Correcta)
-						resolve (xhttp.responseText); // Procesamos la respuesta
+						resolve (JSON.parse (xhttp.responseText)); // Procesamos la respuesta
 					} else {
 						reject (xhttp.statusText); // En caso contrario denegamos el procesamiento y devolvemos la razón
 					};
@@ -27,16 +27,13 @@ let traerDatos = (url_api) => { // Función que incluirá la promesa
 
 traerDatos (API) // Primera llamada a la función con promesa
 .then (datos => { // Procesamos la respuesta obtenida
-	let datosProcesados = JSON.parse (datos); // Procesamos los datos en el JSON recibido
-	console.info (datosProcesados.info.count); // Mostramos en consola la cantidad de personajes en la API de rickandmorty
-	return traerDatos (API + datosProcesados.results[0].id); // Ejecutamos y pasamos la siguiente llamada a la API
+	console.info (datos.info.count); // Mostramos en consola la cantidad de personajes en la API de rickandmorty
+	return traerDatos (API + datos.results[0].id); // Ejecutamos y pasamos la siguiente llamada a la API
 }).then (results => { // Procesamos la respuesta de la segunda llamada
-	let datosProcesados = JSON.parse (results); // Procesamos los datos
-	console.info (datosProcesados.name); // Mostramos en consola el nombre del primer personaje en la API
-	return traerDatos (datosProcesados.origin.url); // Ejecutamos y pasamos la tercer y última llamada
+	console.info (results.name); // Mostramos en consola el nombre del primer personaje en la API
+	return traerDatos (results.origin.url); // Ejecutamos y pasamos la tercer y última llamada
 }).then (origin => { // Procesamos la respuesta de ésta última llamada
-	let datosProcesados = JSON.parse (origin); // Procesamos los datos en la respuesta
-	console.info (datosProcesados.dimension); // Mostramos en consola el nombre de la dimensión del personaje
+	console.info (origin.dimension); // Mostramos en consola el nombre de la dimensión del personaje
 }).catch (error => {
 	console.error (error); // Si ocurre algún error en nuestra cadena de llamadas, mostramos la razón en consola.
 });
